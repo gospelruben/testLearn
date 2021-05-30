201c5f4988edcd16b2f322cf4c0a5b6a8d9113d7
Gospel Ruben <gospelruben.s@gmail.com>
2021-05-30 12:24:49 +0530

    vcs file for Intellij

---
+++ src/test/java/hellochennaiTest.java
@@ -0,0 +1,11 @@
+public class hellochennaiTest {
+
+    public String test = "hello-world";
+
+    public void testHello() {
+        this.test = "hello Chennai!!!";
+        System.out.println(test);
+    }
+
+
+}
