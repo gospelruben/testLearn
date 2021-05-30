a8ae6ee9863004853956e96b0694dd24d35165b2
Gospel Ruben <gospelruben.s@gmail.com>
2021-05-30 11:56:33 +0530

    Initial commit for GitHub Repo

---
+++ src/main/java/helloWorld.java
@@ -0,0 +1,17 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class helloWorld {
+
+    public static String test;
+
+    public static void main(String[] args){
+
+        test="hello world!!";
+
+        System.out.println("hello World!!!!!!!!!!!!!");
+
+        System.out.println(StringUtils.capitalize(test));
+
+    }
+
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,14 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloWorldTest {
+
+    public String test="hello-world";
+
+    public void testHello(){
+        this.test="hello Chennai!!!" ;
+        System.out.println(test);
+    }
+
+
+
+}
